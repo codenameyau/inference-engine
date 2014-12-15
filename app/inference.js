774b8a4d1fb0a4f4be7c9369b608949a122ca332
Jorge Yau <jyau01@citymail.cuny.edu>
2014-12-14 22:54:42 -0500

    Adding tests for addNoun()

--- app/inference.js
+++ app/inference.js
@@ -18,9 +18,24 @@ function InferenceEngine() {
 /**********************************
  * InferenceEngine Public Methods *
  **********************************/
+InferenceEngine.prototype.getGraph = function() {
+  return this.graph;
+};
+
 InferenceEngine.prototype.negate = function(definition) {
   return (definition === true) ? false : true;
 };
 
+InferenceEngine.prototype.inverse = function(noun) {
+  return 'no ' + noun;
+};
+
+InferenceEngine.prototype.addNoun = function(noun, truth) {
+  var inverseNoun = this.inverse(noun);
+  this.graph.addVertex(noun);
+  this.graph.addVertex(inverseNoun);
+  this.graph.addEdge(noun, noun, 1);
+  this.graph.addEdge(inverseNoun, inverseNoun, 1);
+};
 
 module.exports = InferenceEngine;
--- test/test-inference.js
+++ test/test-inference.js
@@ -22,4 +22,41 @@ describe('InferenceEngine', function() {
     });
   });
 
+
+  describe('.addNoun()', function() {
+    var engine = new InferenceEngine();
+    var graph = engine.getGraph();
+    var noun = 'dogs';
+    var inverseNoun = 'no dogs';
+    engine.addNoun(noun);
+
+    it('should add a new vertex to its graph for the noun', function() {
+      assert.isTrue(graph.hasVertex(noun));
+      assert.strictEqual(graph.getSize(), 2);
+    });
+
+    it('should add a new vertex to its graph for not that noun', function() {
+      assert.isTrue(graph.hasVertex(inverseNoun));
+      assert.strictEqual(graph.getSize(), 2);
+    });
+
+    it('should create edge linking dogs to itself with weight of 1', function() {
+      assert.isTrue(graph.hasEdge(noun, noun));
+      assert.strictEqual(graph.getWeight(noun, noun), 1);
+    });
+
+    it('should create edge linking no-dogs to itself with weight of 1', function() {
+      assert.isTrue(graph.hasEdge(inverseNoun, inverseNoun));
+      assert.strictEqual(graph.getWeight(inverseNoun, inverseNoun), 1);
+    });
+
+    it('should not replace any existing nouns with the same name', function() {
+      var nounRefA = graph.getNeighbors(noun);
+      var nounRefB = graph.getNeighbors(inverseNoun);
+      engine.addNoun(noun);
+      assert.deepEqual(graph.getNeighbors(noun), nounRefA);
+      assert.deepEqual(graph.getNeighbors(inverseNoun), nounRefB);
+    });
+  });
+
 });
