0b21eb01945c56c9dc9fb58cd1a434e814d31632
Jorge Yau <jyau01@citymail.cuny.edu>
2014-12-14 21:11:44 -0500

    Passing tests for negate()

--- app/inference.js
+++ app/inference.js
@@ -7,7 +7,20 @@
 
 var Graph = require('directed-graph');
 
+
+/*******************************
+ * InferenceEngine Constructor *
+ *******************************/
 function InferenceEngine() {
   this.graph = new Graph();
 }
 
+/**********************************
+ * InferenceEngine Public Methods *
+ **********************************/
+InferenceEngine.prototype.negate = function(definition) {
+  return (definition === true) ? false : true;
+};
+
+
+module.exports = InferenceEngine;
--- test/test-inference.js
+++ test/test-inference.js
@@ -7,7 +7,19 @@ var assert = chai.assert;
 /*******************************
  * Test Suite: InferenceEngine *
  *******************************/
-
 describe('InferenceEngine', function() {
+
+  // Reusable engine for testing
+  var globalEngine = new InferenceEngine();
+
+  describe('.negate()', function() {
+    it('should return false if the argument is true', function() {
+      assert.isFalse(globalEngine.negate(true));
+    });
+
+    it('should return true if the argument is false', function() {
+      assert.isTrue(globalEngine.negate(false));
+    });
+  });
 
 });
