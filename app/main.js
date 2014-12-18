16328772ddc2ba0bd1b23b8d4aef20ddf1f6a89a
Jorge Yau <jyau01@citymail.cuny.edu>
2014-12-17 23:57:08 -0500

    Adding help message to cli

--- app/main.js
+++ app/main.js
@@ -13,9 +13,23 @@ var InferenceEngine = require('./inference');
   // Create inference engine
   var engine = new InferenceEngine();
 
+  // Define help console message
+  var displayHelpMessage = function() {
+    console.log('\nYou can teach the engine with the following commands:\n');
+    console.log('  all NOUN are NOUN');
+    console.log('  no NOUN are NOUN');
+    console.log('\nYou can ask the engine the following questions:\n');
+    console.log('  are all NOUN NOUN?');
+    console.log('  are no NOUN NOUN?');
+    console.log('\n\nExample:\n');
+    console.log('   > all dogs are mammals');
+    console.log('   > all mammals are animals');
+    console.log('   > are all dogs animals?');
+  };
+
   // Define tab-completion
   var tabCompletion = function(line) {
-    var completions = 'All No are'.split(' ');
+    var completions = 'all no are'.split(' ');
     var hits = completions.filter(function(c) {
       return c.indexOf(line) === 0; });
     return [hits.length ? hits : completions, line];
@@ -34,10 +48,26 @@ var InferenceEngine = require('./inference');
   rl.prompt();
 
   rl.on('line', function(line) {
-    switch (line.trim()) {
+    console.log(line.match(/^all\s+([a-z\s])+\s+are\s+([a-z\s])+\.?$/g));
+    switch (line.trim().toLowerCase()) {
+
+      // Help message
       case 'help':
-        console.log('HELP');
+        displayHelpMessage();
         break;
+
+      // Teach: All NOUN are NOUN
+      case line.match(/^all\s+([a-z\s])+\s+are\s+([a-z\s])+\.?$/g)[0]:
+        // [TODO]
+        console.log('TEACH ALL');
+        break;
+
+      // Teach: No NOUN are NOUN
+      case line.match(/^no\s+([a-z\s])+\s+are\s+([a-z\s])+\.?$/g)[0]:
+        // [TODO]
+        console.log('TEACH NO');
+        break;
+
       default:
         console.log('Hi ' + line);
         break;
@@ -46,7 +76,7 @@ var InferenceEngine = require('./inference');
     rl.prompt();
 
   }).on('close', function() {
-    console.log('Have a great day!');
+    console.log('\nHave a great day!');
     process.exit(0);
   });
 
